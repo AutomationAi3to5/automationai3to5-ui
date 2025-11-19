31e173d0b0d2456158a8ede971edaebdb190af1e
AutomationAi3to5 <stpm5.pt@gmail.com>
2025-11-19 10:32:40 -0500

    Update index.jsx

--- react-ui/src/index.jsx
+++ react-ui/src/index.jsx
@@ -1,1 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import IconShowcase from "./components/IconShowcase";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<IconShowcase />);
 
